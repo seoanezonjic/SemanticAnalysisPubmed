775b3bb686a693f5288a178b41a8b6d2c5a0f1e1
TheWildParamecium <perezgarcia_jesus94@hotmail.com>
2024-04-03 12:19:46 +0200

    Updated state of the project (multi-GPU parallelization added and updated engine template, and moved last part of the workflow to a new template still under development)

--- daemon.sh
+++ daemon.sh
@@ -2,30 +2,33 @@
 . ~soft_bio_267/initializes/init_python
 
 export CURRENT_PATH=`pwd`
-export EXEC_PATH=$FSCRATCH/SemanticAnalysisPubmed/EXECS
 export QUERIES_PATH=$CURRENT_PATH/queries
+export RESULTS_PATH=$CURRENT_PATH/results
 export PUBMED_PATH=/mnt2/fscratch/users/pab_001_uma/pedro/software_data/pubmed
 export MODEL_PATH=$CURRENT_PATH/models
 export MODEL_NAME="all-mpnet-base-v2"
 #export MODEL_NAME="all-MiniLM-L6-v2"
 export CURRENT_MODEL=$MODEL_PATH/$MODEL_NAME
 
-export PYENV=$HOME/py_venvs/llm_env
-
+export PYENV=$HOME/py_venvs/llm_env #TODO: Remove later
 export CODE_PATH=$CURRENT_PATH/scripts #TODO: Remove later
+export PUBMED_CHUNKSIZE=100000
 
-export PUBMED_CHUNKSIZE=200000
+mkdir $RESULTS_PATH
 
-##BEGIN: EXPERIMENTAL: TRYING TO PARALLELIZE 3 PUMBED FILLED BALANCED FOLDERS TO DRISTIBUTE THE EMBEDDING PROCESS IN 3 EXA NODES
+#PATHS TO EXECUTION OF TEMPLATES
+export ENGINE_EXEC_PATH=$FSCRATCH/SemanticAnalysisPubmed/EXECS_engine
+export PROFILES_EXEC_PATH=$FSCRATCH/SemanticAnalysisPubmed/EXECS_profiles
 
+##BEGIN: EXPERIMENTAL: TRYING TO PARALLELIZE 3 PUBMED BALANCED-FILLED FOLDERS TO DRISTIBUTE THE EMBEDDING PROCESS IN 3 EXA NODES
 export PARALLEL_FOLDERS_BASENAME="chunk"
-#export FOLDERS_TO_PARALLELIZE="${PARALLEL_FOLDERS_BASENAME}0;${PARALLEL_FOLDERS_BASENAME}1;${PARALLEL_FOLDERS_BASENAME}2;${PARALLEL_FOLDERS_BASENAME}3"
-export FOLDERS_TO_PARALLELIZE="${PARALLEL_FOLDERS_BASENAME}0;${PARALLEL_FOLDERS_BASENAME}1;${PARALLEL_FOLDERS_BASENAME}2"
+export FOLDERS_TO_PARALLELIZE="${PARALLEL_FOLDERS_BASENAME}0;${PARALLEL_FOLDERS_BASENAME}1;${PARALLEL_FOLDERS_BASENAME}2" #;${PARALLEL_FOLDERS_BASENAME}3" temporally disabled because one exa node is down
 export TSV_FOLDERS=`echo $FOLDERS_TO_PARALLELIZE | tr ";" "-"`
-PARALLEL_FOLDERS_ARRAY=(${FOLDERS_TO_PARALLELIZE//;/ })
-N_PARALLEL_FOLDERS=${#PARALLEL_FOLDERS_ARRAY[@]}
-
+export PARALLEL_FOLDERS_ARRAY=(${FOLDERS_TO_PARALLELIZE//;/ })
+export N_PARALLEL_FOLDERS=${#PARALLEL_FOLDERS_ARRAY[@]}
 ##END: EXPERIMENTAL
+
+export GPU_DEVICES="cuda:0-cuda:1-cuda:2-cuda:3"
 
 if [ "$1" == "download" ] ; then # DOWNLOAD MODEL
         mkdir -p $MODEL_PATH
@@ -34,8 +37,8 @@ if [ "$1" == "download" ] ; then # DOWNLOAD MODEL
 elif [ "$1" == "queries" ] ; then
         mkdir -p $QUERIES_PATH
 	semtools -O HPO -C CNS/HP:0000118 > $QUERIES_PATH/hpo_list
-elif [ "$1" == "engine" ] ; then
-        mkdir -p $EXEC_PATH
+elif [ "$1" == "engine_wf" ] ; then
+        mkdir -p $ENGINE_EXEC_PATH
         source ~soft_bio_267/initializes/init_autoflow
         variables=`echo -e "
                 \\$code_path=$CODE_PATH,
@@ -45,19 +48,31 @@ elif [ "$1" == "engine" ] ; then
                 \\$queries=$QUERIES_PATH/hpo_list,
                 \\$pubmed_path=$PUBMED_PATH,
                 \\$pyenv=$PYENV,
+                \\$gpu_devices=$GPU_DEVICES,
                 \\$parallel_folders_basename=$PARALLEL_FOLDERS_BASENAME,
                 \\$folders_to_parallelize=$FOLDERS_TO_PARALLELIZE,
                 \\$tsv_folder=$TSV_FOLDERS,
                 \\$n_parallel_folders=$N_PARALLEL_FOLDERS,
-                \\$pubmed_chunksize=$PUBMED_CHUNKSIZE        
+                \\$pubmed_chunksize=$PUBMED_CHUNKSIZE,
+                \\$results_path=$RESULTS_PATH       
             " | tr -d [:space:]`
-        AutoFlow -e -w templates/workflow.af -V $variables -o $EXEC_PATH -m 20gb -t 3-00:00:00 -n cal -L $2
-elif [ "$1" == "check" ]; then 
+        AutoFlow -e -w templates/workflow.af -V $variables -o $ENGINE_EXEC_PATH -m 20gb -t 3-00:00:00 -n cal -c 10 -L $2
+elif [ "$1" == "engine_check" ]; then 
 	. ~soft_bio_267/initializes/init_autoflow
-        flow_logger -w -e $EXEC_PATH  -r all $ADD_OPTIONS
-
-elif [ "$1" == "recover" ]; then 
+        flow_logger -w -e $ENGINE_EXEC_PATH  -r all $ADD_OPTIONS
+elif [ "$1" == "engine_recover" ]; then 
 	. ~soft_bio_267/initializes/init_autoflow
         #Make a call to AutoFlow -v if some step of the workflow failed and you want to change the template and dry run to change the sh before running the recover step
-        flow_logger -w -e $EXEC_PATH --sleep 0.1 -l -p $ADD_OPTIONS
+        flow_logger -w -e $ENGINE_EXEC_PATH --sleep 0.1 -l -p $ADD_OPTIONS
+
+elif [ "$1" == "profiles_wf" ]; then
+        mkdir -p $PROFILES_EXEC_PATH
+        source ~soft_bio_267/initializes/init_autoflow
+        variables=`echo -e "
+                \\$suggestions_template=$PWD/templates/reports/suggestions.txt,
+                \\$results_path=$RESULTS_PATH,
+                \\$code_path=$CODE_PATH,
+                \\$pubmed_profiles=$RESULTS_PATH/pmID_profiles.txt
+                " | tr -d [:space:]`
+        AutoFlow -e -w templates/profile_analysis.af -V $variables -o $PROFILES_EXEC_PATH -m 20gb -t 3-00:00:00 -n cal -L $2
 fi
