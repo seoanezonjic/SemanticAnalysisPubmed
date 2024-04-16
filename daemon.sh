232f481c7e24a131518f681378cc71dc769af7fc
TheWildParamecium <perezgarcia_jesus94@hotmail.com>
2024-04-16 21:19:17 +0200

    Updated state of the project (Modified MONDO gold standard preprocessing and moved all reporting parts to specified step of the daemon)

--- daemon.sh
+++ daemon.sh
@@ -12,11 +12,15 @@ export CURRENT_MODEL=$MODEL_PATH/$MODEL_NAME
 #export MODEL_NAME="all-MiniLM-L6-v2"
 
 export PYENV=$HOME/py_venvs/llm_env #TODO: Remove later
-export CODE_PATH=$CURRENT_PATH/scripts #TODO: Remove later
+export CODE_PATH=$CURRENT_PATH/scripts
 export TMP_PATH=$CURRENT_PATH/tmp
 export INPUTS_PATH=$CURRENT_PATH/inputs
 
-mkdir $RESULTS_PATH; mkdir $RESULTS_PATH/reports; mkdir $INPUTS_PATH ; mkdir $TMP_PATH
+export TEMPLATES_PATH=$CURRENT_PATH/templates
+export AUTOFLOW_TEMPLATES=$TEMPLATES_PATH/autoflow
+export REPORTS_TEMPLATES=$TEMPLATES_PATH/reports
+
+mkdir $RESULTS_PATH; mkdir $RESULTS_PATH/reports; mkdir $INPUTS_PATH ; mkdir $TMP_PATH; mkdir $TEMPLATES_PATH; mkdir $AUTOFLOW_TEMPLATES; mkdir $REPORTS_TEMPLATES; mkdir $QUERIES_PATH
 export PATH=$CODE_PATH:$PATH
 
 #PATHS TO EXECUTION OF TEMPLATES
@@ -35,7 +39,7 @@ export N_PARALLEL_FOLDERS=${#PARALLEL_FOLDERS_ARRAY[@]}
 
 #OTHER VARIABLES
 export PUBMED_CHUNKSIZE=300000
-export MIN_SIMILARITY=0.85
+export MIN_SIMILARITY=0.80
 #export GPU_DEVICES="cuda:0-cuda:1-cuda:2-cuda:3"
 export GPU_DEVICES="cuda:0-cuda:1-cuda:2-cuda:3-cuda:4-cuda:5-cuda:6-cuda:7"
 export database_ids="OMIM ORPHA"
@@ -46,20 +50,22 @@ if [ "$1" == "download" ] ; then # DOWNLOAD MODEL and other resources
         #Download model
         mkdir -p $MODEL_PATH
         source $PYENV/bin/activate #TODO: Remove later
-        stEngine -m $MODEL_NAME -p $CURRENT_MODEL
+        stEngine -m $MODEL_NAME -p $CURRENT_MODEL -v
 
         #Download MONDO-PUMBED relations and MONDO-HP relations
         wget https://data.monarchinitiative.org/latest/tsv/all_associations/publication_disease.all.tsv.gz -O $TMP_PATH/publication_disease.all.tsv.gz
         wget http://purl.obolibrary.org/obo/hp/hpoa/phenotype.hpoa -O $TMP_PATH/phenotype.hpoa
 
         #Process MONDO-PUMBED relations
-        zcat tmp/publication_disease.all.tsv.gz | tail -n +2 | cut -f 1,5 | grep MONDO | aggregate_column_data -i - -x 2 -a 1 > $INPUTS_PATH/mondo_pubmed_profiles.txt
+        zcat tmp/publication_disease.all.tsv.gz | tail -n +2 | cut -f 1,5 | grep MONDO | grep PMID | sed "s/PMID://g" | \
+                aggregate_column_data -i - -x 2 -a 1 > $INPUTS_PATH/mondo_pubmed_profiles.txt
         #Process MONDO-HP relations
         prepare_mondo_hp_relations.sh
 
 elif [ "$1" == "queries" ] ; then
         mkdir -p $QUERIES_PATH
 	semtools -O HPO -C CNS/HP:0000118 > $QUERIES_PATH/hpo_list
+
 elif [ "$1" == "engine_wf" ] ; then
         mkdir -p $ENGINE_EXEC_PATH
         source ~soft_bio_267/initializes/init_autoflow
@@ -78,39 +84,70 @@ elif [ "$1" == "engine_wf" ] ; then
                 \\$n_parallel_folders=$N_PARALLEL_FOLDERS,
                 \\$pubmed_chunksize=$PUBMED_CHUNKSIZE,
                 \\$results_path=$RESULTS_PATH,
-                \\$report_templates_path=$CURRENT_PATH/templates/reports       
+                \\$report_templates_path=$REPORTS_TEMPLATES       
             " | tr -d [:space:]`
-        AutoFlow -e -w templates/workflow.af -V $variables -o $ENGINE_EXEC_PATH -m 20gb -t 3-00:00:00 -n cal -c 10 -L $2
-elif [ `echo $1 | cut -f 2 -d "_"` == "check" ]; then 
-	. ~soft_bio_267/initializes/init_autoflow
-        path_to_check=$(grep `echo $1 | cut -f 1 -d "_"` $CURRENT_PATH/workflow_paths | cut -f 2)
-        echo $path_to_check
-        flow_logger -w -e $path_to_check  -r all $ADD_OPTIONS
-elif [ `echo $1 | cut -f 2 -d "_"` == "recover" ]; then 
-	. ~soft_bio_267/initializes/init_autoflow
-        path_to_recover=$(grep `echo $1 | cut -f 1 -d "_"` $CURRENT_PATH/workflow_paths | cut -f 2)
-        #Make a call to AutoFlow -v if some step of the workflow failed and you want to change the template and dry run to change the sh before running the recover step
-        flow_logger -w -e $path_to_recover --sleep 0.1 -l -p $ADD_OPTIONS
+        AutoFlow -e -w $AUTOFLOW_TEMPLATES/workflow.af -V $variables -o $ENGINE_EXEC_PATH -m 20gb -t 3-00:00:00 -n cal -c 10 -L $2
 
 elif [ "$1" == "bench_wf" ]; then
         mkdir -p $PROFILES_EXEC_PATH
         source ~soft_bio_267/initializes/init_autoflow
+        cut -f 1,2 $RESULTS_PATH/llm_pmID_profiles_with_cosine_sim.txt > $RESULTS_PATH/llm_pmID_profiles.txt
 
-        cut -f 1,2 $RESULTS_PATH/pmID_profiles.txt > $RESULTS_PATH/pmID_profiles_without_cosine_sim.txt
+        #Get LLM and MONDO common pmIDs
+        desaggregate_column_data -i $INPUTS_PATH/mondo_pubmed_profiles.txt -x 2 | aggregate_column_data -i - -x 2 -a 1 > $TMP_PATH/reverse_aggregated_mondo_pmid_profiles.txt
+        intersect_columns -a $TMP_PATH/reverse_aggregated_mondo_pmid_profiles.txt -b $RESULTS_PATH/llm_pmID_profiles.txt -A 1 -B 1 --k c --full |\
+                cut -f 1,2 | desaggregate_column_data -i - -x 2 | aggregate_column_data -i - -x 2 -a 1 | sort -u > $INPUTS_PATH/mondo_pubmed_profiles_common_pmids.txt
 
-        RECORDS_NUMBER=`wc -l $INPUTS_PATH/mondo_hpo_profiles.txt | cut -f 1 -d " "`
-        PACKAGE_SIZE=500
+        #Get MONDO IDs with both HPO and pmID profiles
+        intersect_columns -a $INPUTS_PATH/mondo_pubmed_profiles_common_pmids.txt -b $INPUTS_PATH/mondo_hpo_profiles.txt -A 1 -B 1 --k c --full > $TMP_PATH/mondo_pmids_and_hpos 
+        cut -f 1,2 $TMP_PATH/mondo_pmids_and_hpos | sort -u > $INPUTS_PATH/MONDO_PMIDs_cleaned
+        cut -f 3,4 $TMP_PATH/mondo_pmids_and_hpos | sort -u > $INPUTS_PATH/MONDO_HPOs_cleaned
+        cut -f 1 $TMP_PATH/mondo_pmids_and_hpos > $TMP_PATH/mondo_PMID_HPO_common_IDs
+
+        #Get the number of records to process and the package size
+        RECORDS_NUMBER=`wc -l $TMP_PATH/mondo_PMID_HPO_common_IDs | cut -f 1 -d " "`
+        PACKAGE_SIZE=50
         PACKAGE_NUMBER=`echo "$RECORDS_NUMBER / $PACKAGE_SIZE" | bc`
 
         variables=`echo -e "
                 \\$pyenv=$PYENV,
                 \\$results_path=$RESULTS_PATH,
                 \\$code_path=$CODE_PATH,
-                \\$mondo_pubmed_profiles=$INPUTS_PATH/mondo_pubmed_profiles.txt,
-                \\$mondo_hpo_profiles=$INPUTS_PATH/mondo_hpo_profiles.txt,
-                \\$llm_pmID_profiles=$RESULTS_PATH/pmID_profiles_without_cosine_sim.txt,
+                \\$mondo_pmid_hpo_common_ids=$TMP_PATH/mondo_PMID_HPO_common_IDs,
+                \\$mondo_pmID_profiles=$INPUTS_PATH/MONDO_PMIDs_cleaned,
+                \\$mondo_hpo_profiles=$INPUTS_PATH/MONDO_HPOs_cleaned,
+                \\$llm_pmID_profiles=$RESULTS_PATH/llm_pmID_profiles.txt,
                 \\$package_number="0-$PACKAGE_NUMBER",
-                \\$package_size=$PACKAGE_SIZE
+                \\$package_size=$PACKAGE_SIZE,
+                \\$report_templates_path=$REPORTS_TEMPLATES
                 " | tr -d [:space:]`
-        AutoFlow -e -w templates/disease_benchmarking.af -V $variables -o $PROFILES_EXEC_PATH -m 4gb -t 3-00:00:00 -n cal -c 2 -L $2
+        AutoFlow -e -w $AUTOFLOW_TEMPLATES/disease_benchmarking.af -V $variables -o $PROFILES_EXEC_PATH -m 4gb -t 3-00:00:00 -n cal -c 2 -L $2
+
+elif [ "$1" == "reports" ]; then
+        count_mondo_entries.sh
+        #stEngine reports
+        get_report_pubmed_index.py -d $RESULTS_PATH/abstracts_debug_stats.txt \
+                                   -t $REPORTS_TEMPLATES/debug_report_get_pubmed_index.txt \
+                                   -o $RESULTS_PATH/reports/debug_report_get_pubmed_index
+
+        report_html -d $RESULTS_PATH/llm_filtered_scores \
+                    -t $REPORTS_TEMPLATES/debug_report_semantic_scores_stats.txt \
+                    -o $RESULTS_PATH/reports/debug_report_semantic_scores_stats
+
+        #Benchmark reports
+        report_html -t $REPORTS_TEMPLATES/llm_mondo_similitudes.txt \
+                    -d $RESULTS_PATH/llm_vs_mondo_semantic_similarity_hpo_based.txt \
+                    -o $RESULTS_PATH/reports/llm_vs_mondo_similitudes
+
+elif [ `echo $1 | cut -f 2 -d "_"` == "check" ]; then 
+	. ~soft_bio_267/initializes/init_autoflow
+        path_to_check=$(grep `echo $1 | cut -f 1 -d "_"` $CURRENT_PATH/workflow_paths | cut -f 2)
+        echo $path_to_check
+        flow_logger -w -e $path_to_check  -r all $ADD_OPTIONS
+
+elif [ `echo $1 | cut -f 2 -d "_"` == "recover" ]; then 
+	. ~soft_bio_267/initializes/init_autoflow
+        path_to_recover=$(grep `echo $1 | cut -f 1 -d "_"` $CURRENT_PATH/workflow_paths | cut -f 2)
+        #Make a call to AutoFlow -v if some step of the workflow failed and you want to change the template and dry run to change the sh before running the recover step
+        flow_logger -w -e $path_to_recover --sleep 0.1 -l -p $ADD_OPTIONS
 fi
--- scripts/get_report_pubmed_index.py
+++ scripts/get_report_pubmed_index.py
@@ -1,8 +1,31 @@
 #! /usr/bin/env python
 
 from py_report_html import Py_report_html
-import sys, os
+import sys, os, argparse
+from collections import defaultdict
 
+############################################################################################
+## OPTPARSE
+############################################################################################
+parser = argparse.ArgumentParser(description=f'Usage: {os.path.basename(__file__)} [options]')
+
+parser.add_argument("-d", "--data_file", dest="data_file", default= None,
+                    help="Path to file containing the abstract related stats returned from xml abstracts parsing")
+
+parser.add_argument("-t", "--template_file", dest="template_file", default= None,
+                    help="Path to the template file to be used for the report generation")
+
+parser.add_argument("-o", "--output_file", dest="output_file", default= None, 
+                    help="Path to the output file to be generated with the report")
+
+opts = parser.parse_args()
+options = vars(opts)
+
+#################################################################################
+## MAIN
+#################################################################################
+
+#Warning line example where stats are extracted:
 #file=/mnt2/fscratch/users/pab_001_uma/pedro/software_data/pubmed/pubmed24n0881.xml.gz,total=30000,no_abstract=4562,no_pmid=0
 file_raw_stats = []
 file_proportion_stats = []
@@ -13,7 +36,7 @@ all_no_abstract = 0
 all_no_pmid = 0
 
 counter = -1
-with open(sys.argv[1]) as f:
+with open(options["data_file"]) as f:
     for line in f:
         stats = line.strip().split('stats:')[1]
         stats = stats.split(',')
@@ -52,7 +75,7 @@ total_stats = [["rows", "all_total", "no_abstract", "no_pmid", "abstract"], ["ro
 total_proportion_stats = [["rows", "proportion_no_abstract", "proportion_no_pmid", "proportion_abstract"], ["row 0", all_no_abstract_proportion, all_no_pmid_proportion, all_abstract_proportion]]
 
 container = {'file_raw_stats' : file_raw_stats, 'file_proportion_stats' : file_proportion_stats, 'total_stats' : total_stats, 'total_proportion_stats' : total_proportion_stats}
-template = open(sys.argv[2]).read()
-report = Py_report_html(container, "debug_report_get_pubmed_index", True, True)
+template = open(options["template_file"]).read()
+report = Py_report_html(container, options["output_file"], True, True)
 report.build(template)
-report.write('debug_report_get_pubmed_index.html')
+report.write(f'{options["output_file"]}.html')
